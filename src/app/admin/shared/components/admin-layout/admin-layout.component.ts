6376bd2ce90b3240d524ad98c3ece5f5ee10a739
vladimir <lovkiy2012@gmail.com>
2020-05-15 22:24:29 +0300

    135 structure admin layout

--- src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,15 +1,17 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss']
+  styleUrls: ['./admin-layout.component.scss'],
 })
 export class AdminLayoutComponent implements OnInit {
+  constructor(private router:Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  logout(event: Event) {
+    event.preventDefault();
+    this.router.navigate(['/admin','login'])
   }
-
 }
