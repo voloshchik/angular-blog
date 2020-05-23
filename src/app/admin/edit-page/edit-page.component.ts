ce9f5a868bf33000aa5bcaa7937b590439f9b8dd
vladimir <lovkiy2012@gmail.com>
2020-05-23 18:11:59 +0300

    150 post editor page

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { PostService } from '../../shared/post.service';
+import { switchMap } from 'rxjs/operators';
+import { Post } from '../../shared/components/interfaces';
+import { Validators } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
-  styleUrls: ['./edit-page.component.scss']
+  styleUrls: ['./edit-page.component.scss'],
 })
 export class EditPageComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private postServece: PostService
+  ) {}
 
   ngOnInit(): void {
+    this.route.params
+      .pipe(
+        switchMap((params: Params) => {
+          return this.postServece.getById(params['id']);
+        })
+      )
+      .subscribe((post: Post) => {
+        console.log('post', post)
+        this.form = new FormGroup({
+          title: new FormControl(post.title,Validators.required),
+          text: new FormControl(post.text,Validators.required),
+        });
+      });
   }
+  submit(){
 
+  }
 }
--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -39,4 +39,16 @@ export class PostService {
   remove(id: string): Observable<void> {
     return this.http.delete<void>(`${environment.fbDbUrl}/posts/${id}.json`);
   }
+
+  getById(id: string): Observable<Post> {
+    return this.http.get<Post>(`${environment.fbDbUrl}/posts/${id}.json`).pipe(
+      map((post: Post) => {
+        return {
+          ...post,
+          id,
+          date: new Date(post.date),
+        };
+      })
+    );
+  }
 }
